1b6fe732869c65b07020875f3675f72596b674c9
bjemtj <34427747+bjemtj@users.noreply.github.com>
2020-03-13 23:57:50 +0700

    Update event.simulate.js

--- event.simulate.js
+++ event.simulate.js
@@ -10,55 +10,64 @@
  **/
 (function(){
   
+  function simulate(element, eventName)
+  {
+      var options = extend(defaultOptions, arguments[2] || {});
+      var oEvent, eventType = null;
+
+      for (var name in eventMatchers)
+      {
+          if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+      }
+
+      if (!eventType)
+          throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+      if (document.createEvent)
+      {
+          oEvent = document.createEvent(eventType);
+          if (eventType == 'HTMLEvents')
+          {
+              oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+          }
+          else
+          {
+              oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+              options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+              options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+          }
+          element.dispatchEvent(oEvent);
+      }
+      else
+      {
+          options.clientX = options.pointerX;
+          options.clientY = options.pointerY;
+          var evt = document.createEventObject();
+          oEvent = extend(evt, options);
+          element.fireEvent('on' + eventName, oEvent);
+      }
+      return element;
+  }
+
+  function extend(destination, source) {
+      for (var property in source)
+        destination[property] = source[property];
+      return destination;
+  }
+
   var eventMatchers = {
-    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
-    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+      'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+      'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
   }
   var defaultOptions = {
-    pointerX: 0,
-    pointerY: 0,
-    button: 0,
-    ctrlKey: false,
-    altKey: false,
-    shiftKey: false,
-    metaKey: false,
-    bubbles: true,
-    cancelable: true
+      pointerX: 0,
+      pointerY: 0,
+      button: 0,
+      ctrlKey: false,
+      altKey: false,
+      shiftKey: false,
+      metaKey: false,
+      bubbles: true,
+      cancelable: true
   }
-  
-  Event.simulate = function(element, eventName) {
-    var options = Object.extend(Object.clone(defaultOptions), arguments[2] || { });
-    var oEvent, eventType = null;
-    
-    element = $(element);
-    
-    for (var name in eventMatchers) {
-      if (eventMatchers[name].test(eventName)) { eventType = name; break; }
-    }
-
-    if (!eventType)
-      throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
-
-    if (document.createEvent) {
-      oEvent = document.createEvent(eventType);
-      if (eventType == 'HTMLEvents') {
-        oEvent.initEvent(eventName, options.bubbles, options.cancelable);
-      }
-      else {
-        oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView, 
-          options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
-          options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
-      }
-      element.dispatchEvent(oEvent);
-    }
-    else {
-      options.clientX = options.pointerX;
-      options.clientY = options.pointerY;
-      oEvent = Object.extend(document.createEventObject(), options);
-      element.fireEvent('on' + eventName, oEvent);
-    }
-    return element;
-  }
-  
-  Element.addMethods({ simulate: Event.simulate });
 })();
