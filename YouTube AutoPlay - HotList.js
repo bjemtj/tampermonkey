4db864920a64f851ef68025a2d3f3158eed065d3
bjemtj <34427747+bjemtj@users.noreply.github.com>
2020-03-14 01:08:16 +0700

    add files

---
+++ YouTube AutoPlay - HotList.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         YouTube AutoPlay - HotList
+// @version      2.0.0
+// @description  This script Autoplay Youtube
+// @author       bjemtj
+// @match        *music.youtube.com/hotlist*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function doFunc_click(elements){
+        let rdPos = Math.floor(Math.random()*elements.length);
+        elements[rdPos].click();
+    }
+    function doAtLeastOnce(selector, time, doFunc){
+        let interval = setInterval(function(){
+            let elements = document.querySelectorAll(selector);
+            if(elements.length > 0){
+                clearInterval(interval);
+                doFunc(elements);
+            }
+        }, time);
+    }
+    function run() {
+        console.log("YouTube AutoPlay - HotList");
+        let selector = 'ytmusic-item-thumbnail-overlay-renderer';
+        doAtLeastOnce(selector, 5000, doFunc_click);
+    };
+
+    setTimeout(run, 5000);
+
+})();
